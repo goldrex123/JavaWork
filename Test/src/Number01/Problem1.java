b7294f47572b244a3ea7bb9a05c4a4708e3fc4dc
admin <admin@H25>
2020-05-06 10:26:52 +0900

    200506-1

--- Test/src/Number01/Problem1.java
+++ Test/src/Number01/Problem1.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class Problem1 {
 
 	public static void main(String[] args) {
+		String s = "AB";
+		int n = 1;
+		String answer = "";
+
+		char c[] = s.toCharArray();
 		
-		double num1;
-		double num2;
+		for(int i = 0 ; i<c.length; i++){
+			if(c[i] != ' '){
+				c[i] = (char)(c[i] + n);
+			}
+		}
+
+		answer = String.valueOf(c);
+		System.out.println(answer);
 		
-		Scanner sc =new Scanner(System.in);
 		
-		num1 = sc.nextDouble();
-		num2 = sc.nextDouble();
-		
-		System.out.println(String.format("%.1f", (num1+num2)));
-		System.out.println(String.format("%.1f", (num1*num2)));
-		
-		sc.close();
 	}
 
 }
