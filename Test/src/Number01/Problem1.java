8693d3e5333364b73a83d45e1e0f3a3db299441f
admin <admin@H25>
2020-05-06 17:35:09 +0900

    2020-05-06 커밋1

--- Test/src/Number01/Problem1.java
+++ Test/src/Number01/Problem1.java
@@ -5,22 +5,33 @@ import java.util.Scanner;
 public class Problem1 {
 
 	public static void main(String[] args) {
-		String s = "AB";
+		String s = "a B ";
 		int n = 1;
-		String answer = "";
-
 		char c[] = s.toCharArray();
+		int num = 0;
+        boolean isUpper = false;
+        
+		for(int i = 0;i<c.length;i++){
+            if(c[i] >= 'a' && c[i] <='z'){
+                c[i] = (char)(c[i] + n);
+            } else if(c[i] >= 'A' && c[i] <='Z'){
+                c[i] = (char)(c[i] + n);
+                isUpper = true;
+            }
+                
+            if(!Character.isLetter(c[i])){
+                if(isUpper){
+                    num = c[i]-'Z'-1;
+                    c[i] = 'A';
+                    c[i] = (char)(c[i]+num);
+                } else {
+                    num = c[i]-'z'-1;
+                    c[i] = 'a';
+                    c[i] = (char)(c[i]+num);
+                }
+            }
+            isUpper = false;
+        }
 		
-		for(int i = 0 ; i<c.length; i++){
-			if(c[i] != ' '){
-				c[i] = (char)(c[i] + n);
-			}
-		}
-
-		answer = String.valueOf(c);
-		System.out.println(answer);
-		
-		
-	}
-
+	}	
 }
