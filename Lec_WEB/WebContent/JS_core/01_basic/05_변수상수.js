2526ad277fbb45cd51175301a8b5aabd6baee9a6
kimjunyoung <goldrex123@naver.com>
2020-05-07 17:51:35 +0900

    2020-05-07 커밋1

---
+++ Lec_WEB/WebContent/JS_core/01_basic/05_변수상수.js
@@ -0,0 +1,47 @@
+var a = 100;
+console.log('a= ', a)
+a= 200
+console.log('a= ', a)
+
+var a = 500 //중복 선언 가능
+console.log('a= ', a)
+
+// ES6 부터 let, const로 변수 상수 지정
+let value = 177;
+console.log('value=', value);
+value= 555;
+console.log('value=', value);
+
+//중복 선언 불가! 동일 이름 변수 let으로 선언 불가.
+//let value= 277; 
+
+//상수 선언
+const b = 1;
+console.log('b = ', b)
+//b == 2 오류!
+
+
+//--------------------------------------------
+// var는 오늘날 js 환경에서는 그닥 권장하지는 않음
+
+// IE9, IE10  와 같은 구형 브라우저에서는
+//  let, const 를 사용 못함.
+
+// 개발 단계에서는 '바벨' 등을 사용하여
+// 우리가 개발한 코드가 구형 브라우저 에서동 동작케 함.
+
+let c; //초기화를 따로해도 가능.
+c = 100;
+
+//` : back tick
+console.log(`c = ${c}`);
+
+// 데이터 타입들
+let value2 = 100;
+console.log(value2, typeof value2);
+console.log(value2, typeof(value2));
+
+//JS는 대입되는 값에 따라 타입이 바뀜
+value2 = 'hello';
+console.log(value2, typeof value2);
+value2 = "Hello" // '~', "~" 둘다 가능
--- Test/src/Number01/Problem1.java
+++ Test/src/Number01/Problem1.java
@@ -1,37 +1,47 @@
 package Number01;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Problem1 {
 
 	public static void main(String[] args) {
-		String s = "a B ";
-		int n = 1;
-		char c[] = s.toCharArray();
-		int num = 0;
-        boolean isUpper = false;
-        
-		for(int i = 0;i<c.length;i++){
-            if(c[i] >= 'a' && c[i] <='z'){
-                c[i] = (char)(c[i] + n);
-            } else if(c[i] >= 'A' && c[i] <='Z'){
-                c[i] = (char)(c[i] + n);
-                isUpper = true;
-            }
-                
-            if(!Character.isLetter(c[i])){
-                if(isUpper){
-                    num = c[i]-'Z'-1;
-                    c[i] = 'A';
-                    c[i] = (char)(c[i]+num);
-                } else {
-                    num = c[i]-'z'-1;
-                    c[i] = 'a';
-                    c[i] = (char)(c[i]+num);
-                }
-            }
-            isUpper = false;
-        }
-		
+		Problem1 pb1 = new Problem1();
+		String[] str =  {"BACDE", "CBADF", "AECB", "BDA"};
+		pb1.solution("CBD", str);
 	}	
+	
+	 public int solution(String skill, String[] skill_trees) {
+	        int answer = 0;
+	        
+	        String c[] = skill.split("");
+	        List<String> list = new ArrayList<String>();
+	        list.addAll(Arrays.asList(c));
+	        boolean ga = false;
+	        int num;
+	        
+	        for(int i = 0 ; i < skill_trees.length; i++){
+	            ArrayList<String> list2 = new ArrayList<String>();
+	            list2.addAll(Arrays.asList(skill_trees[i].split("")));
+	            
+	            for(int j = 0 ; j< list2.size(); j++){
+	                
+	                if(!list.contains(list2.get(j))){
+	                    list2.remove(j);
+	                    j--;
+	                }
+	            }
+	            
+	            for(num = 0 ; num < list2.size(); num++){
+	                if(!(list.indexOf(list2.get(num))==num)) break;
+	            }
+	            if(num == list2.size()) ga = true;
+	            num = 0;
+	            if(ga) answer++;
+	            ga = false;
+	        }
+	        System.out.println(answer);
+	        return answer;
+	    }	
 }
