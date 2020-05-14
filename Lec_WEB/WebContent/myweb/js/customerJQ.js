afae73235cbd0b1467a56b190dfac71fed134896
kimjunyoung <goldrex123@naver.com>
2020-05-14 17:45:13 +0900

    2020-05-14 커밋1

--- Lec_WEB/WebContent/myweb/js/customerJQ.js
+++ Lec_WEB/WebContent/myweb/js/customerJQ.js
@@ -1,8 +1,29 @@
+
+
 $(document).ready(function(){
+
     $(".questionBtn").click(function(){
-        var str = "$("+ this + ") ~ " + span;
-        console.log(str);
-        $(str).css({display : "inline-block"})
-        $(".explainDiv").css({margin: 0})
+        var ximg = $(" .ximg",this);
+        var ex = $('+ .explainDiv .explain',this);
+        
+        
+        if(ex.css('display') == 'none'){
+            var state = true;
+        } else{
+            var state = false;
+        }
+        
+        if(state){
+            ximg.css('transform', 'rotate(0deg)');
+            state = false;
+        } else {
+            ximg.css('transform', 'rotate(45deg)');
+            state = true;
+        }
+        ex.css('margin-bottom', '10px');
+        ex.slideToggle("fast");
+        $('.explain').not(ex).css('display', 'none');
+        
+        $(".explainDiv").css('margin-top', 0)
     });
 });
