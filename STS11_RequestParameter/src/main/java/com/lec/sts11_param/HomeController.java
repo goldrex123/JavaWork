c7dfbb0f3d57221db1044189422662cd89e8b535
kimjunyoung <goldrex123@naver.com>
2020-06-30 16:10:00 +0900

    2020-06-30 커밋ㄱ

--- STS11_RequestParameter/src/main/java/com/lec/sts11_param/HomeController.java
+++ STS11_RequestParameter/src/main/java/com/lec/sts11_param/HomeController.java
@@ -10,9 +10,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lec.beans.WriteDTO;
 
@@ -42,70 +45,90 @@ public class HomeController {
 	}
 	
 	// parameter 추출
-	// handler 메소드에서도 서블릿에서 보았던 HttpServletRequest, HttpServletResponse 매개변수 가능
-	@RequestMapping(value="/member/delete")
-	public String delMember(HttpServletRequest request, Model model) {
+	// handler 메소드에서도 서블릿에서 보앗던 HttpServletRequest, HttpServletResponse 매개변수 가능.!!!
+	
+	@RequestMapping(value = "/member/delete", method = RequestMethod.GET)   // -> /member/delete?id=34
+	//public String delMember(Model model, HttpServletRequest request) {
+	public String delMember(HttpServletRequest request, Model model) {  // 순서 바꾸어도 동작!
 		String id = request.getParameter("id");
-		model.addAttribute("mbId", id + ",");
+		model.addAttribute("mbId", id);
 		return "member/delete";
 	}
 	
-	@RequestMapping(value="/member/regOk", method = RequestMethod.POST)
+	@RequestMapping(value = "/member/regOk", method = RequestMethod.POST)
 	public String regOkMember() {
 		System.out.println("/member/regOk : POST");
 		return "member/regOk";
 	}
-	
-	@RequestMapping(value="/member/regOk", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/member/regOk", method = RequestMethod.GET)
 	public String regOkMember(Model model) {
 		System.out.println("/member/regOk : GET");
 		return "member/regOk";
 	}
 	
-	@RequestMapping(value="/member/regist")
+	@RequestMapping(value = "/member/regist")
 	public String registMember() {
 		return "member/regist";
 	}
 	
-	// get / post 둘다 받는 handler
-	@RequestMapping(value="/member/regOk2", method= {RequestMethod.GET, RequestMethod.POST} )
+	// GET/POST 둘다 받는 handler
+	@RequestMapping(value = "/member/regOk2", 
+			method = {RequestMethod.GET, RequestMethod.POST})
 	public String regOkMember2() {
 		return "member/regOk";
 	}
 	
-	//handler 에
-	//reqeust parameter 의 name 값과 '같은 이름의 매개변수' 가 있으면
-	// 바로 그 매개변수가 reqeust parameter 값을 받아온다
-	//public String findMember(int id, String name, Model model) { // 숫자 타입은 바로 parsing 해서 받는다
-//	@RequestMapping("/member/find")
-//	public String findMember(Model model, String[] name, int [] id) {
+	// handler 에
+	// request parameter 의 name 값과 '같은 이름의 매개변수' 가 있으면
+	// 바로 그 매개변수가 request parameter 값을 받아온다.
+	
+	//@RequestMapping("/member/find")
+	//public String findMember(String id, String name, Model model) {
+	//public String findMember(Model model, String name, int id) {  // 순서 무관!
+	
+	// 숫자타입으로 하면 바로 parsing 하여 받는다.
+	//public String findMember(Model model, String name, double id) {
+	
+	// 동일한 name의 request parameter 들.
+//	public String findMember(Model model, String [] name, int [] id) {
 //		model.addAttribute("id", Arrays.toString(id));
 //		model.addAttribute("name", Arrays.deepToString(name));
 //		
 //		return "member/find";
 //	}
 	
+	
+	// @RequestParam 사용
 	@RequestMapping("/member/find")
-	public String findMember(Model model, @RequestParam("id") String userid, @RequestParam("name") String username) {
+	public String findMember(Model model,
+			@RequestParam("id") String userid,   // name "id" parameter 값을 받아온다.
+			@RequestParam("name") String username) {
+		
 		model.addAttribute("id", userid);
 		model.addAttribute("name", username);
 		
-		return "member/find";
+		return "member/find";		
 	}
 	
-	//------------------------------------
-	// 커맨드 객체 사용
+	//---------------------------
+	// 커맨드 객체 (Command Object) 사용
+	
 	@RequestMapping("/board/write")
 	public String writeBoard() {
 		return "board/write";
 	}
 	
-	// 기존 방식으로 구현하기
-	// 매 parameter 들을 매개변수화 해야한다. bad
-//	@RequestMapping(value="/board/writeOk", method = RequestMethod.POST)	
-//	public String writeOkBoard(Model model, @RequestParam("name") String name, @RequestParam("subject") String subject,
-//			@RequestParam("content") String content) {
-//		
+	
+	// 기존방식으로 구현하기
+	// 매 parameter 들을 매개변수화? 해야 한다. 힘들다.
+	@RequestMapping(value = "/board/writeOk", method= RequestMethod.POST)
+//	public String writeOkBoard(
+//			Model model, 
+//			@RequestParam("name") String name,
+//			@RequestParam("subject") String subject,
+//			@RequestParam("content") String content
+//			) {
 //		WriteDTO dto = new WriteDTO();
 //		dto.setName(name);
 //		dto.setSubject(subject);
@@ -113,26 +136,70 @@ public class HomeController {
 //		
 //		model.addAttribute("dto", dto);
 //		
-//		return "board/writeOk";
+//		return "board/writeOk";		
 //	}
+	// 커맨드 객체 사용
+	//public String writeOkBoard(WriteDTO dto){
 	
-	@RequestMapping(value="/board/writeOk", method = RequestMethod.POST)	
-	public String writeOkBoard(WriteDTO dto) {
+	// 커맨드 객체에 attribute id 변경
+	public String writeOkBoard(
+			@ModelAttribute("DTO") WriteDTO dto){
 		
-		return "board/writeOk";
+		System.out.println(dto);
+		return "board/writeOk";		
 	}
+	
+	// @PathVariable 방식
+	@RequestMapping("/board/writePath/{name}/{subject}/{content}")
+	public String writePathBoard(Model model,
+			@PathVariable String name,
+			@PathVariable String subject,
+			@PathVariable String content
+			) {
+		model.addAttribute("name", name);
+		model.addAttribute("subject", subject);
+		model.addAttribute("content", content);
+		return "board/writepath";
+	}
+	  
+	@RequestMapping("/member/ageCheck")
+	public String chkAge(int age, RedirectAttributes redirectAttr) {
+		redirectAttr.addAttribute("age", age);
+		
+		if(age < 19) {
+			return "redirect:/member/underAge";
+		} else {
+			return "redirect:/member/adult";
+		}
+	}
+	
+	@RequestMapping("/member/underAge")
+	public String pageUnderAge(
+			@RequestParam("age") int age, Model model
+			) {
+		model.addAttribute("age", age);
+		return "member/ageUnder";
+	}
+	
+	@RequestMapping("/member/adult")
+	public String pageAdult(
+			@RequestParam("age") int age, Model model
+			) {
+		model.addAttribute("age", age);
+		return "member/ageAdult";
+	}
+	
+	
+	@RequestMapping(value = "/common")   //  /common 으로 요청이 오면
+	public String cccmmm() {        // cccmmm() 핸들러가 수행되고.
+		return "comn";     // -->  /WEB-INF/views/comn.jsp   를 리턴하여 response 되게 한다.
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- STS11_RequestParameter/src/main/webapp/WEB-INF/views/board/write.jsp
+++ STS11_RequestParameter/src/main/webapp/WEB-INF/views/board/write.jsp
@@ -8,38 +8,63 @@
 <title>글작성</title>
 </head>
 <script>
-function chkSubmit(){ // 폼 검증
+function chkSubmit(){  // 폼 검증
 	frm = document.forms["frm"];
 	
-	var name = frm['name'].value.trim();
-	var subject = frm['subject'].value.trim();
+	var name = frm["name"].value.trim();
+	var subject = frm["subject"].value.trim();
 	
 	if(name == ""){
 		alert("작성자 란은 반드시 입력해야 합니다");
-		frm['name'].focus();
+		frm["name"].focus();
 		return false;
 	}
-	if(subject==""){
+	if(subject == ""){
 		alert("제목은 반드시 작성해야 합니다");
 		frm["subject"].focus();
 		return false;
 	}
+	return true;
 }
-</script>
 
+</script>
 <body>
 <h2>글작성</h2>
 <%-- 글 내용이 많을수 있기 때문에 POST 방식 사용 --%>
 <form name="frm" action="writeOk" method="post" onsubmit="return chkSubmit()">
+
+<%-- int 매개변수 받는 setter 도 동작할까? --%>
+uid: 
+<input type="text" name="uid" value="123"/><br>
+
 작성자:
-<input type="text" name="name" value="김준영"/><br>
+<input type="text" name="name" value="장윤성"/><br>
 제목:
 <input type="text" name="subject" value="와 점심이다"/><br>
 내용:<br>
-<textarea name="content">맛있네</textarea>
+<textarea name="content">마있져요.</textarea>
 <br><br>
 <input type="submit" value="등록"/>
 </form>
 <br>
+<button type="button" onclick="location.href='list.jsp'">목록으로</button>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
